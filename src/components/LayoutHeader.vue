eac3053d0037348b3a5879fb619ab22f07005946
claudia.sollinger@gmail.com <claudia.sollinger@gmail.com>
2022-11-13 20:20:44 +0100

    FEAT: first step design LayoutHeader

---
+++ src/components/LayoutHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Todo App</h1>
+    <p>Get your things done</p>
+  </header>
+</template>
+
+<script>
+export default { name: "LayoutHeader" };
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-accent-pink);
+  padding-block: 10px;
+}
+h1 {
+  color: var(--color-bg);
+  margin: 0;
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LayoutHeader />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LayoutHeader from "@/components/LayoutHeader.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    LayoutHeader,
   },
 };
 </script>
